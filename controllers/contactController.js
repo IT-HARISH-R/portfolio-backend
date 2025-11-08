d382f26794401204ea1a6fad1661023d26aecdf6
IT-HARISH-R <harishmass27.8@gmail.com>
2025-11-08 14:16:25 +0530

    add console

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -4,17 +4,22 @@ import sendEmail from '../utlis/email.js';
 export const submitContactForm = async (req, res) => {
     try {
         const { name, email, message } = req.body;
-        // console.log({ name, email, message })
+        console.log({ name, email, message })
+
         if (!name || !email || !message) {
             return res.status(400).json({ message: 'All fields are required' });
         }
-
+        console.log("All fields done")
         const newContact = new Contact({ name, email, message });
+        console.log("add database done")
 
         await sendEmail({ name, email, message });
+        console.log("Send mail done")
 
 
         await newContact.save();
+        console.log("Cntact Save done")
+        console.log("Send REsponse done")
 
         res.status(201).json({ message: 'Message sent successfully' });
     } catch (error) {
